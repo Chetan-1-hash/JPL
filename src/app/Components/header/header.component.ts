2c5eb7282efaece1ba9f5a97bf5cc0e6af72dfcc
Chetan <chetan17302@gmail.com>
2024-12-28 13:15:33 +0530

    Improved responsiveness

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faNavicon } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, FontAwesomeModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  faNav = faNavicon;
+  isOpened:boolean = false;
+
+  @ViewChild('navrouter') navrouter!: ElementRef;
+
+  openNav() {
+    this.isOpened = !this.isOpened;
+  }
 
 }
