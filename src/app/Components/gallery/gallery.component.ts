e759e662584bd4fc14cb24787ce41061d2a9f862
Chetan <chetan17302@gmail.com>
2025-01-01 10:11:29 +0530

    Added animation effects and resized the tables

--- src/app/Components/gallery/gallery.component.ts
+++ src/app/Components/gallery/gallery.component.ts
@@ -10,35 +10,42 @@ import { FormsModule } from '@angular/forms';
 })
 export class GalleryComponent {
 
-  year:number=2024;
-  
-  imageLists_2021:string[]=[
+  year: number = 2024;
+
+  imageLists_2021: string[] = [
     "IMG_20211115_083912.jpg",
-    "IMG-20210127-WA0003.jpg", 
+    "IMG-20210127-WA0003.jpg",
     "IMG-20210127-WA0004.jpg",
-    "IMG-20210127-WA0005.jpg"
+    "IMG-20210127-WA0005.jpg",
+    "IMG-20210127-WA0005.jpg",
+    "IMG-20210127-WA0005.jpg",
+    "IMG-20210127-WA0005.jpg",
+    "IMG-20210127-WA0005.jpg",
+    "IMG-20210127-WA0005.jpg",
+    "IMG-20210127-WA0005.jpg",
   ];
-  
-  imageLists_2022:string[]=[];
-  imageLists_2023:string[]=[];
-  imageLists_2024:string[]=[];
 
-  imgLists:string[]=this.imageLists_2024;
+  imageLists_2022: string[] = [];
+  imageLists_2023: string[] = [];
+  imageLists_2024: string[] = [];
+
+  imgLists: string[] = this.imageLists_2024;
 
 
-  YearChange(event:any){
+  YearChange(event: any) {
     this.year = +event?.target.value;
-    if(this.year===2021){
-      this.imgLists=this.imageLists_2021
-    }
-    else if(this.year===2022){
-      this.imgLists=this.imageLists_2022
-    }
-    else if(this.year===2023){
-      this.imgLists=this.imageLists_2023
-    }
-    else if(this.year===2024){
-      this.imgLists=this.imageLists_2024
+    if (this.year === 2021) {
+      this.imgLists = []; // Clear the list temporarily for animation
+      setTimeout(() => this.imgLists = this.imageLists_2021, 200);
+    } else if (this.year === 2022) {
+      this.imgLists = [];
+      setTimeout(() => this.imgLists = this.imageLists_2022, 200);
+    } else if (this.year === 2023) {
+      this.imgLists = [];
+      setTimeout(() => this.imgLists = this.imageLists_2023, 200);
+    } else if (this.year === 2024) {
+      this.imgLists = [];
+      setTimeout(() => this.imgLists = this.imageLists_2024, 0);
     }
   }
 
